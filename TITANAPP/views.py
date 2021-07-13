c3b8015edcb205b96131bc31283b5f0075f870c7
P-ws <pws12232002@gmail.com>
2021-07-13 10:02:53 +0900

    모델에 저장해서 다 저장된 데이터베이스 저장

--- TITANAPP/views.py
+++ TITANAPP/views.py
@@ -2,14 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from TITANAPP.models import NewModel
+
 
 def TITAN_Introduce (requset):
     if requset.method == "POST":
+        temp = requset.POST.get('input_text')
+        model_instance = NewModel()
+        model_instance.text = temp
+        model_instance.save()
+
+        data_list = NewModel.objects.all()
         return render(requset, 'TITANAPP/middle.html',
-                      context={'text':'POST METHOD!'})
+                      context={'data_list': data_list})
     else:
+        data_list = NewModel.objects.all()
         return render(requset, 'TITANAPP/middle.html',
-                      context={'text':'GET METHOD!'})
+                      context={'data_list': data_list})
 
 
 
